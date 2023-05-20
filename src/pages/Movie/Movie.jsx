99c3f1ecd2b167467d131c680aab94f0248ccdfd
Olena <anyanyme@gmail.com>
2023-05-20 14:45:56 +0200

    update Movies

--- src/pages/Movie/Movie.jsx
+++ src/pages/Movie/Movie.jsx
@@ -1,8 +1,8 @@
-import React, { useRef } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React from 'react';
+
 import MovieCard from 'components/MovieCard/MovieCard';
-import css from './Movie.module.css'
-import { CgArrowTopLeftO } from 'react-icons/cg';
+
+
 
 export const Movie = () => {
   
